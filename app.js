5da0d38eeb7013cd4cb9821bb7e7c98f8ff13fdb
Musa Thanmay Sadguru <thanmaysadguru5912@gmail.com>
2024-10-20 22:58:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const {open} = require('sqlite');
 const sqlite3 = require('sqlite3');
 const path = require('path');
-
+const port = process.env.PORT || 5000;
 const app=express();
 app.use(cors());
 app.use(express.json());
